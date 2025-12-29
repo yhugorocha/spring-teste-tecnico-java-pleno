a63236a634ba7cd090d35390f964b2352f746a73
Hugo Silva <jose.silva@extreme.digital>
2025-12-29 17:28:44 -0300

    feat: create migration coupon

---
+++ src/main/resources/db/migration/V01__create_table_coupon.sql
@@ -0,0 +1,11 @@
+CREATE TABLE coupon (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(6) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    discount_value DECIMAL(5,2) NOT NULL,
+    expiration_date DATE NOT NULL,
+    is_published BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE UNIQUE INDEX uk_coupon_code ON coupon (code);
