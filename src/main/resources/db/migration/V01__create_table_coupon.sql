c3d791ab28d16cac011b844605b6a6fda604ba71
Hugo Silva <jose.silva@extreme.digital>
2025-12-29 20:07:45 -0300

    feat: Adding new column in the entity

--- src/main/resources/db/migration/V01__create_table_coupon.sql
+++ src/main/resources/db/migration/V01__create_table_coupon.sql
@@ -5,6 +5,7 @@ CREATE TABLE coupon (
     discount_value DECIMAL(5,2) NOT NULL,
     expiration_date DATE NOT NULL,
     is_published BOOLEAN NOT NULL DEFAULT FALSE,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
